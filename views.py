678a41c59d5b5f3b30ee947e4b8f3ee7023054a4
Bigmanbiggiey <91302183+Bigmanbiggiey@users.noreply.github.com>
2024-07-15 11:41:22 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.views.decorators.http import require_POST
+
+from .models import Task
+# Create your views here.
+@require_POST
+def logout_view(request):
+    if request.user.is_authenticated:
+     logout(request)
+
+    return redirect('login')
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    #return HttpResponse('To Do list')
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
